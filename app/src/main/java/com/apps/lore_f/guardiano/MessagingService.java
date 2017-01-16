32d82ba8857f9ffdd4201ee2bfb340c6ac74332c
lorenzofailla <lorenzofailla.developer@gmail.com>
2017-01-16 22:38:44 +0100

    avanzamento

--- app/src/main/java/com/apps/lore_f/guardiano/MessagingService.java
+++ app/src/main/java/com/apps/lore_f/guardiano/MessagingService.java
@@ -56,7 +56,7 @@ public class MessagingService extends FirebaseMessagingService {
         Log.d(TAG, "From: " + remoteMessage.getFrom());
         Log.d(TAG, "Message: " + remoteMessage.getNotification().getBody());
 
-        /*
+
         // Check if message contains a data payload.
         if (remoteMessage.getData().size() > 0) {
             Log.d(TAG, "Message data payload: " + remoteMessage.getData());
@@ -66,7 +66,7 @@ public class MessagingService extends FirebaseMessagingService {
         if (remoteMessage.getNotification() != null) {
             Log.d(TAG, "Message Notification Body: " + remoteMessage.getNotification().getBody());
         }
-        */
+
 
         switch(remoteMessage.getNotification().getBody()){
 
